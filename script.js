df7846ac23070b9d8dfd3b0432b58b8ef47490fc
Mahadev Metre <mahadev.metre666@gmail.com>
2025-09-25 17:19:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function orderNow(product) {
-    let message = `Hi Mom's Kitchen, I want to order: ${product}`;
+    let message = `Hi Swaad Sutra, I want to order: ${product}`;
     let whatsappNumber = "919999999999"; // replace with real number
     let whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappLink, "_blank");
